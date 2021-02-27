9749f633776c18c6af9a4be1fa8a1cf3ac5d46aa
Ibrahim Mamdouh <ibrahim.mamdouh@coformatique.com>
2021-02-27 21:54:49 +0200

    test changes

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const p = 4969;
-let gen_count = 0;
+// let gen_count = 0;
+// const orders = {};
+const generators = {};
 for (let a = 1; a < p; a++) {
   let x = 1;
   let i = 0;
@@ -9,9 +11,26 @@ for (let a = 1; a < p; a++) {
     i++;
   } while (x !== 1);
 
-  if (i === p - 1) gen_count++;
+  // orders[i] = (orders[i] || 0) + 1;
+  if (i === p - 1) {
+    // console.log(a);
+    generators[a] = true;
+  }
 }
+
+console.log(generators);
 
 // Number of generators in Z*_p = Phi(|Z*_p|)
 
-console.log(`Number of generators in Z*_${p} = `, gen_count);
+// console.log(`Number of generators in Z*_${p} = `, gen_count);
+
+// console.log(Object.values(generators).length);
+
+// const tries = 1000000;
+// let isPrem = 0;
+// for (let i = 0; i < tries; i++) {
+//   const rand = Math.floor(1 + Math.random() * p);
+//   if (generators[rand]) isPrem++;
+// }
+
+// console.log(isPrem / tries);
